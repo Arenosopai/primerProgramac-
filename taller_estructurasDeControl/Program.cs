016b56980587350c961dc2891d5991391d5db9e5
Arenosopai <arenassebastian364@gmail.com>
2025-10-17 12:56:07 -0500

    taller1

---
+++ taller_estructurasDeControl/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taller_estructurasDeControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //taller ejercicio 1
+
+            //const float INTERESESAnual = 0.05f;
+            //Console.WriteLine("Escribe el monto del prestamo: ");
+            //float prestamo = float.Parse(Console.ReadLine());
+            //float intanual = prestamo * INTERESESAnual;
+            //Console.WriteLine($"su interes anual del 5% es: {intanual} ");
+            //float trimestral = intanual / 4;
+            //Console.WriteLine($"su interes trimestral es: {trimestral} es decir que al tercer trimestres es: {trimestral+trimestral+trimestral}");
+            //float mensual = intanual / 12;
+            //Console.WriteLine($"su interes mensual es {mensual}");
+            //float total = prestamo + (intanual*5);
+            //Console.WriteLine($"su total a pagar con intereses en 5 años es: {total}");
+
+            //taller ejercicio 2
+
+            //Console.WriteLine("Escribe valor de su pago mensual: ");
+            //int pago_de_empleado = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Escribe valor de su ahorro mensual: ");
+            //float ahorro_de_empleado = float.Parse(Console.ReadLine());
+            //Console.WriteLine($"su pago mensual es de: {pago_de_empleado} $");
+            //Console.WriteLine($"su ahorro mensual es de: {ahorro_de_empleado} $");
+            //float salud = pago_de_empleado * 0.125f;
+            //float pension = pago_de_empleado * 0.16f;
+            //float total = pago_de_empleado - ahorro_de_empleado - salud - pension;
+            //Console.WriteLine($"el total que vas a recibir descontando su ahorro mensual, pension y salud es: {total} ");
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
